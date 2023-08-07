205f67946ad8c874951dac5f811ee0b9128f6631
ivan <ivanitchoo.ivanalbano9@gmail.com>
2023-08-07 22:26:46 +0200

    added my skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function Skills() {
-    const skills = ['TypeScript', 'React', 'Angular', 'HTML', 'CSS', 'Vite'];
+    const skills = ['Vanilla JavaScript', 'PHP', 'HTML', 'CSS', 'Node.JS', 'Python', 'Flask', 'MySQL'];
 
-    const devtools = ['Bootstrap ', 'Tailwind', 'Github', 'Codepen', 'Terminal', 'Vercel', 'Netlify'];
+    const devtools = ['Bootstrap ', 'Tailwind', 'Git', 'SandBox', 'Render', 'Vercel', 'Replit', 'PlanetScale', ];
     
     return (
         <>
@@ -12,15 +12,16 @@ function Skills() {
                     Skills
                 </h1>
                 <p className="tracking-wide text-gray-600 dark:text-gray-400 mb-16">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente voluptates provident in assumenda quisquam repudiandae, labore doloremque corporis exercitationem placeat nesciunt dolore sit suscipit reiciendis aliquam qui. Earum, eligendi modi.
+                Results-orientated web developer with a strong emphasis on effective problem solving throughout the 
+                development process. Knowledge of Linux systems and skilled in system design and UI engineering.
                 </p>
                 <div className='mt'>
-                    <p>Here are a few technologies i work with:
+                    <p>Technologies I Work With:
                         <ul className="skills-list">
                             {skills.map((skill, i) => <li key={i} className="relative mb-5 pl-6">{skill}</li>)}
                         </ul>
                     </p><br />
-                    <p>Here are few of my Dev tools:
+                    <p>Development Tools:
                         <ul className="skills-list">
                             {devtools.map((tools, i) => <li key={i} className="relative mb-5 pl-6">{tools}</li>)}
                         </ul>

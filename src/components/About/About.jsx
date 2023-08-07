3a7b19963062da275de5ddd5eaaa9503b7473c7f
ivan <ivanitchoo.ivanalbano9@gmail.com>
2023-08-07 22:00:38 +0200

    wrote my custom about info

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,15 +11,18 @@ function About() {
                             Ivan L. Albano Mangunyane
                         </h1>
                         <h2 className="tracking-wide text-gray-700 dark:text-gray-200 mb-4">
-                            IT | WEB DEVELOPER{' '}
-                            <span className="font-semibold">Developer</span>
+                            IT | WEB {' '}
+                            <span className="font-bold">DEVELOPER</span>
                         </h2>
-                        <p className="tracking-wide text-gray-600 dark:text-gray-400 mb-16">
-                            Hey, I'm a Frontend Developer at Company. I enjoy working with React Js and crafting beautiful front-end experiences.
+                        <p className="tracking-wide text-gray-600 dark:text-gray-400 mb-16 text-justify">
+                        I'm a dedicated Web Developer based in Maputo, Mozambique. I have skills in both frontend and backend technologies. 
+                        I have a strong passion for football, with <a href='https://www.fcporto.pt/pt' target='_blank'>FC Porto</a>&#128153;
+                        & <a href='https://www.manutd.com/' target='_blank'>Manchester United</a>&#10084; being my favorites. I enjoy going to
+                        the gym and playing Call of Duty Warzone &#x1F60D;&#x1F3AE; 
                         </p>
                     </div>
-                    <div className="w-[80px] sm:w-[206px] relative mb-8 sm:mb-0 mr-auto">
-                        <img src="https://avatars.githubusercontent.com/u/101179443?v=4" alt="avatar" title="avatar" className="rounded-full grayscale hover:grayscale-0" />
+                    <div className="w-[80px] sm:w-[500px] relative mb-8 sm:mb-0 mr-auto">
+                        <img src="src\components\pic\ivan.jpg" alt="avatar" title="avatar" className="rounded-full grayscale hover:grayscale-0" />
                     </div>
                 </div>
 
@@ -27,8 +30,41 @@ function About() {
                     <h1 className="font-bold text-2xl md:text-2xl mb-1 text-black dark:text-white">
                         About Me
                     </h1>
-                    <p className="tracking-wide text-gray-600 dark:text-gray-400 mb-16">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos architecto consequuntur sunt, exercitationem quia quaerat veniam vero possimus repellendus, recusandae neque? Velit ex culpa distinctio accusantium aliquid, ab odio aspernatur. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis molestias atque, distinctio assumenda est rerum error velit amet explicabo vero eaque inventore, iste illo soluta laboriosam facere animi minus eos!
+                    <p className="tracking-wide text-gray-600 dark:text-gray-400 mb-16 text-justify">
+
+                        <p>
+                        I studied computer and telecommunications engineering at ISUTC. I am currently working as an IT 
+                        at Sociedade Moçambicana das Participações, SA. 
+                        </p><br></br>
+
+                        <p>
+                            I'm a web developer whose journey began in college, where I discovered my knack for creating 
+                            dynamic web pages that interact with databases perfectly. Although I initially studied Java 
+                            programming in college, I found it complex and challenging, especially when building websites with JSP.
+                        </p><br></br>
+                        
+                        <p>
+                            Determined to simplify the process, I explored other languages like PHP and explored back-end frameworks
+                            like Node.Js and Flask, which made development faster. I now plan to learn how to create web pages with the 
+                            React.Js framework as a way to express my creative abilities.
+                        </p> <br></br>
+
+                        <p>
+                            As I continue on this exciting path, I am tirelessly enhancing my skills by dedicating myself to learning new 
+                            technologies every day. I pride myself on my proficiency in frontend and server-side development, and aspire 
+                            to specialize as a data analyst. 
+                        </p><br></br>
+
+                        <p>
+                            My passion for data and programming is beyond measure, at a time when strategic decisions can have a significant 
+                            impact on a nation's economy. I fuel the ambition to enhance my skills in data handling, from collection and cleaning 
+                            to analysis and visualization.
+                        </p><br></br>
+
+                        <p> 
+                            By mastering big data techniques, I aim to empower myself to handle massive volumes of complex information, enabling 
+                            valuable insights and strategic information for informed decision-making.
+                        </p>
                     </p>
                 </div>
                 <Skills />
